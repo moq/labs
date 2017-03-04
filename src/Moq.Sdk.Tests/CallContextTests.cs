1374f138a25ec69119b41138e12d3eb1d1353f9e
Daniel Cazzulino <daniel@cazzulino.com>
2017-03-04 18:23:41 -0300

    Added CallContext to pass data out of band for a call
    This is needed for the upcoming fluent behaviors in Moq, like
    foo.Title = "Hello" to work. The property setter would set the
    last invocation in the CallContext and that will be picked up
    by the assignment to set the expected return value from that
    point on.

---
+++ src/Moq.Sdk.Tests/CallContextTests.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Moq.Sdk.Tests
+{
+    class CallContextTests
+    {
+        [Fact]
+        public void WhenFlowingData_ThenCanUseContext()
+        {
+            var key1 = Guid.NewGuid().ToString();
+            var d1 = new object();
+            var t1 = default(object);
+            var t10 = default(object);
+            var t11 = default(object);
+            var t12 = default(object);
+            var t13 = default(object);
+
+            var key2 = Guid.NewGuid().ToString();
+            var d2 = new object();
+            var t2 = default(object);
+            var t20 = default(object);
+            var t21 = default(object);
+            var t22 = default(object);
+            var t23 = default(object);
+
+            Task.WaitAll(
+                Task.Run(() =>
+                {
+                    CallContext.SetData(key1, d1);
+                    new Thread(() => t10 = CallContext.GetData(key1)).Start();
+                    Task.WaitAll(
+                        Task.Run(() => t1 = CallContext.GetData(key1))
+                            .ContinueWith(t => t11 = CallContext.GetData(key1)),
+                        Task.Run(() => t12 = CallContext.GetData(key1)),
+                        Task.Run(() => t13 = CallContext.GetData(key1))
+                    );
+                }),
+                Task.Run(() =>
+                {
+                    CallContext.SetData(key2, d2);
+                    new Thread(() => t20 = CallContext.GetData(key2)).Start();
+                    Task.WaitAll(
+                        Task.Run(() => t2 = CallContext.GetData(key2))
+                            .ContinueWith(t => t21 = CallContext.GetData(key2)),
+                        Task.Run(() => t22 = CallContext.GetData(key2)),
+                        Task.Run(() => t23 = CallContext.GetData(key2))
+                    );
+                })
+            );
+
+            Assert.Same(d1, t1);
+            Assert.Same(d1, t10);
+            Assert.Same(d1, t11);
+            Assert.Same(d1, t12);
+            Assert.Same(d1, t13);
+
+            Assert.Same(d2, t2);
+            Assert.Same(d2, t20);
+            Assert.Same(d2, t21);
+            Assert.Same(d2, t22);
+            Assert.Same(d2, t23);
+
+            Assert.Null(CallContext.GetData(key1));
+            Assert.Null(CallContext.GetData(key2));
+        }
+
+        [Fact]
+        public void WhenFlowingData_ThenCanUseGenericContext()
+        {
+            var key1 = Guid.NewGuid().ToString();
+            var d1 = new Foo();
+            var t1 = default(Foo);
+            var t10 = default(Foo);
+            var t11 = default(Foo);
+            var t12 = default(Foo);
+            var t13 = default(Foo);
+
+            var key2 = Guid.NewGuid().ToString();
+            var d2 = new Foo();
+            var t2 = default(Foo);
+            var t20 = default(Foo);
+            var t21 = default(Foo);
+            var t22 = default(Foo);
+            var t23 = default(Foo);
+
+            Task.WaitAll(
+                Task.Run(() =>
+                {
+                    CallContext<Foo>.SetData(key1, d1);
+                    new Thread(() => t10 = CallContext<Foo>.GetData(key1)).Start();
+                    Task.WaitAll(
+                        Task.Run(() => t1 = CallContext<Foo>.GetData(key1))
+                            .ContinueWith(t => t11 = CallContext<Foo>.GetData(key1)),
+                        Task.Run(() => t12 = CallContext<Foo>.GetData(key1)),
+                        Task.Run(() => t13 = CallContext<Foo>.GetData(key1))
+                    );
+                }),
+                Task.Run(() =>
+                {
+                    CallContext<Foo>.SetData(key2, d2);
+                    new Thread(() => t20 = CallContext<Foo>.GetData(key2)).Start();
+                    Task.WaitAll(
+                        Task.Run(() => t2 = CallContext<Foo>.GetData(key2))
+                            .ContinueWith(t => t21 = CallContext<Foo>.GetData(key2)),
+                        Task.Run(() => t22 = CallContext<Foo>.GetData(key2)),
+                        Task.Run(() => t23 = CallContext<Foo>.GetData(key2))
+                    );
+                })
+            );
+
+            Assert.Same(d1, t1);
+            Assert.Same(d1, t10);
+            Assert.Same(d1, t11);
+            Assert.Same(d1, t12);
+            Assert.Same(d1, t13);
+
+            Assert.Same(d2, t2);
+            Assert.Same(d2, t20);
+            Assert.Same(d2, t21);
+            Assert.Same(d2, t22);
+            Assert.Same(d2, t23);
+
+            Assert.Null(CallContext<Foo>.GetData(key1));
+            Assert.Null(CallContext<Foo>.GetData(key2));
+        }
+
+        public class Foo { }
+    }
+}
---
+++ src/Moq.Sdk/CallContext.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Moq.Sdk
+{
+    public static class CallContext<T>
+    {
+        static ConcurrentDictionary<string, AsyncLocal<T>> state = new ConcurrentDictionary<string, AsyncLocal<T>>();
+
+        /// <summary>
+        /// Stores a given object and associates it with the specified name.
+        /// </summary>
+        /// <param name="name">The name with which to associate the new item in the call context.</param>
+        /// <param name="data">The object to store in the call context.</param>
+        public static void SetData(string name, T data) => 
+            state.GetOrAdd(name, _ => new AsyncLocal<T>()).Value = data;
+
+        /// <summary>
+        /// Retrieves an object with the specified name from the <see cref="CallContext"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the data being retrieved. Must match the type used when the <paramref name="name"/> was set via <see cref="SetData{T}(string, T)"/>.</typeparam>
+        /// <param name="name">The name of the item in the call context.</param>
+        /// <returns>The object in the call context associated with the specified name, or a default value for <typeparamref name="T"/> if none is found.</returns>
+        public static T GetData(string name) =>
+            state.TryGetValue(name, out AsyncLocal<T> data) ? data.Value : default(T);
+    }
+
+
+    /// <summary>
+    /// Provides a way to set contextual data that flows with the call and 
+    /// async context of a test or invocation.
+    /// </summary>
+    public static class CallContext
+    {
+        static ConcurrentDictionary<string, AsyncLocal<object>> state = new ConcurrentDictionary<string, AsyncLocal<object>>();
+
+        /// <summary>
+        /// Stores a given object and associates it with the specified name.
+        /// </summary>
+        /// <param name="name">The name with which to associate the new item in the call context.</param>
+        /// <param name="data">The object to store in the call context.</param>
+        public static void SetData(string name, object data) =>
+            state.GetOrAdd(name, _ => new AsyncLocal<object>()).Value = data;
+
+        /// <summary>
+        /// Retrieves an object with the specified name from the <see cref="CallContext"/>.
+        /// </summary>
+        /// <param name="name">The name of the item in the call context.</param>
+        /// <returns>The object in the call context associated with the specified name, or <see langword="null"/> if not found.</returns>
+        public static object GetData(string name) =>
+            state.TryGetValue(name, out AsyncLocal<object> data) ? data.Value : null;
+    }
+}
