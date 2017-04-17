a5af7eb239ba99f79f708279eed6891bcd0e87ff
Daniel Cazzulino <daniel@cazzulino.com>
2017-04-17 17:04:25 -0300

    Default proxy factory retrieves proxies from the specified assembly
    This factory basically replicates the naming scheme of the proxy generator,
    and retrieves the proxies from the specified assembly.

--- src/Moq.Proxy/IProxyFactory.cs
+++ src/Moq.Proxy/IProxyFactory.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Moq.Proxy
 {
@@ -10,6 +12,7 @@ namespace Moq.Proxy
         /// <summary>
         /// Creates a proxy with the given parameters.
         /// </summary>
+        /// <param name="proxiesAssembly">Optional assembly where compile-time generated proxies exist.</param>
         /// <param name="baseType">The base type (or main interface) of the proxy.</param>
         /// <param name="implementedInterfaces">Optional additional interfaces to implement in the proxy.</param>
         /// <param name="construtorArguments">
@@ -17,6 +20,6 @@ namespace Moq.Proxy
         /// is a class, rather than an interface.
         /// </param>
         /// <returns>A proxy that implements <see cref="IProxy"/> in addition to the configured interfaces (if any).</returns>
-		object CreateProxy(Type baseType, Type[] implementedInterfaces, object[] construtorArguments);
+		object CreateProxy(Assembly proxiesAssembly, Type baseType, IEnumerable<Type> implementedInterfaces, object[] construtorArguments);
 	}
 }
--- src/Moq.Proxy/ProxyFactory.cs
+++ src/Moq.Proxy/ProxyFactory.cs
@@ -1,15 +1,33 @@
-﻿namespace Moq.Proxy
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Moq.Proxy
 {
     /// <summary>
     /// Allows accessing the default <see cref="IProxyFactory"/> to use to 
     /// create proxies.
     /// </summary>
-    public static class ProxyFactory
+    public class ProxyFactory : IProxyFactory
     {
         /// <summary>
         /// Gets or sets the default <see cref="IProxyFactory"/> to use 
         /// to create proxies.
         /// </summary>
-        public static IProxyFactory Default { get; set; }
+        public static IProxyFactory Default { get; set; } = new ProxyFactory();
+
+        private ProxyFactory() { }
+
+        /// <summary>
+        /// See <see cref="IProxyFactory.CreateProxy(Assembly, Type, IEnumerable{Type}, object[])"/>
+        /// </summary>
+        public object CreateProxy(Assembly proxiesAssembly, Type baseType, IEnumerable<Type> implementedInterfaces, object[] construtorArguments)
+        {
+            var name = "ProxyOf" + baseType.Name + string.Join("", implementedInterfaces.Select(x => x.Name));
+            var type = proxiesAssembly.GetType(name, true, false);
+
+            return Activator.CreateInstance(type, construtorArguments);
+        }
     }
 }
