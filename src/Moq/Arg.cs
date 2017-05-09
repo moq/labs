f8000476171f53c0290f9768b92203a812065271
Daniel Cazzulino <daniel@cazzulino.com>
2017-05-09 01:12:48 -0300

    Added property notation for Any, renamed Is->Any
    Having Any(condition) as an overload to Any() seems more
    intuitive.
    
    Also added an Arg<T>.Any property notation for cases where
    that syntax is preferred to a method call.

--- src/Moq/Arg.cs
+++ src/Moq/Arg.cs
@@ -4,8 +4,15 @@ using Moq.Sdk;
 
 namespace Moq
 {
+    /// <summary>
+    /// Allows matching arguments in an invocation.
+    /// </summary>
     public static class Arg
     {
+        /// <summary>
+        /// Matches any value of the given type.
+        /// </summary>
+        /// <typeparam name="T">The type of the argument.</typeparam>
         public static T Any<T>()
         {
             CallContext<Stack<IArgumentMatcher>>.GetData(nameof(IArgumentMatcher), () => new Stack<IArgumentMatcher>())
@@ -14,12 +21,37 @@ namespace Moq
             return default(T);
         }
 
-        public static T Is<T>(Func<T, bool> condition)
+        /// <summary>
+        /// Matches a value of the given type if it satisfies the specified condition.
+        /// </summary>
+        /// <typeparam name="T">The type of the argument.</typeparam>
+        /// <param name="condition">The condition to check against actual invocation values.</param>
+        public static T Any<T>(Func<T, bool> condition)
         {
             CallContext<Stack<IArgumentMatcher>>.GetData(nameof(IArgumentMatcher), () => new Stack<IArgumentMatcher>())
                 .Push(new IsArgumentMatcher<T>(condition));
 
             return default(T);
+        }
+    }
+
+    /// <summary>
+    /// Allows matching arguments of the given type <typeparamref name="T"/> 
+    /// in an invocation.
+    /// </summary>
+    public static class Arg<T>
+    {
+        /// <summary>
+        /// Matches any argument value with a matching type.
+        /// </summary>
+        public static T Any
+        {
+            get
+            {
+                CallContext<Stack<IArgumentMatcher>>.GetData(nameof(IArgumentMatcher), () => new Stack<IArgumentMatcher>())
+                    .Push(AnyArgumentMatcher<T>.Default);
+                return default(T);
+            }
         }
     }
 }
