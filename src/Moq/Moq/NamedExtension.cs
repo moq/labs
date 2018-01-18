4c81ae6ef347ea6689d5638df82355b7e22c88a3
Daniel Cazzulino <daniel@cazzulino.com>
2018-01-18 04:04:49 -0300

    Added ability to name mocks
    Just persist a value in the mock' State :).
    
    One way this state could be consumed in the near
    feature is via C# 8's "extension everything", which
    would make extension properties possible (among
    other things).

---
+++ src/Moq/Moq/NamedExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Moq.Properties;
+using Moq.Sdk;
+
+namespace Moq
+{
+    /// <summary>
+    /// Extension for naming mocks.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    public static class NamedExtension
+    {
+        /// <summary>
+        /// Names the mock.
+        /// </summary>
+        public static T Named<T>(this T target, string name)
+        {
+            var mock = (target is IMocked mocked) ?
+                mocked.Mock : throw new ArgumentException(Resources.TargetNotMock);
+
+            mock.State.Set("Name", name);
+            return target;
+        }
+    }
+}
