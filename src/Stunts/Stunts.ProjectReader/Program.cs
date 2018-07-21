277e53816a48bf1ba6ca51228d169176fac48ad0
Daniel Cazzulino <daniel@cazzulino.com>
2018-07-21 00:20:17 -0300

    Boost performance by reusing MSBuildWorkspace in project metadata reader
    The external process should be longer lived so that the MSBuildWorkspace.Create
    call (by far the most expensive since it needs to set up a whole new MEF host
    with all Roslyn services, in addition to resolving the MSBuild framework assemblies
    and all SDK-style targets and assemblies too) can be reused across calls to
    ReadProject (renamed to OpenProject).
    
    By switching to the JsonRpc style, the external process can live as long as the
    build (or the AppDomain?) and maintain a single instance of the MSBuildWorkspace,
    which then is reused across OpenProject calls. The RPC interface exposed allows
    creating new workspaces on demand (disposing the existing one automatically, so
    that it can live for the entire duration of the VS AppDomain eventually.
    
    In addition, the console app now triggers a background resolving of all the
    relevant assemblies and MEF eagerly, so that by the time the actual project
    reading happens, all of the Roslyn components have already warmed up.
    
    This combination of optimizations brought down the time for reading the test
    project and all its project references from 20+ seconds (with the single-use
    commandline for each project) to ~5 seconds (not counting the warm-up period
    of ~2 seconds).

--- src/Stunts/Stunts.ProjectReader/Program.cs
+++ src/Stunts/Stunts.ProjectReader/Program.cs
@@ -4,169 +4,110 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using Microsoft.Build.Locator;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.MSBuild;
-using Mono.Options;
+using StreamJsonRpc;
 
 namespace Stunts
 {
     internal class Program
     {
-        public static async Task<int> Main(string[] args)
+        private static ManualResetEventSlim exit = new ManualResetEventSlim();
+        private static bool ready;
+
+        public static int Main(string[] args)
         {
-            var help = false;
-            var instance = default(VisualStudioInstance);
-            var msbuild = default(string);
-            var wait = false;
-            var project = default(string);
-            var properties = new Dictionary<string, string>();
-            var configuration = default(string);
-            var platform = default(string);
-            var output = default(string);
+            if (args.Any(s => s.Equals("-d")) || args.Any(s => s.Equals("/d")))
+                Debugger.Launch();
 
-            var options = new OptionSet
-            {
-                { "p|project=", "MSBuild project file to read", p => project = p },
-                { "i|installation:", "Visual Studio installation directory", v => instance =
-                    MSBuildLocator.QueryVisualStudioInstances(new VisualStudioInstanceQueryOptions { DiscoveryTypes = DiscoveryType.VisualStudioSetup })
-                    .FirstOrDefault(x => x.VisualStudioRootPath.Equals(v, StringComparison.OrdinalIgnoreCase)) ??
-                    throw new ArgumentException($"Did not find a Visual Studio installation at {v}.") },
-                { "m|msbuild:", "MSBuild root directory.", m => msbuild = m },
-                { "c|configuration=", "Project configuration (i.e. Debug or Release)", c => configuration = c },
-                { "t|platform=", "Project target platform (i.e. AnyCPU)", plat => platform = plat },
-                { "o|output:", "Optional output file to emit. Emits to standard output if not specified", o => output = o },
-                { "<>", "Additional MSBuild global properties to set", v =>
-                    {
-                        var values = v.Split(new[] { '=', ':' }, 2);
-                        properties[values[0].Trim()] = values[1].Trim().Trim('\"');
-                    }
-                },
-                new ResponseFileSource(),
-                { "d|debug", "Attach a debugger to the process", d => Debugger.Launch() },
-                { "w|wait", "Wait before exiting the process", w => wait = w != null },
-                { "h|?|help", "Show this message and exit", h => help = true },
-            };
+            if (args.Length != 0 && Directory.Exists(args[0]))
+                // If we receive a path as a first argument, we use that for MSBuild
+                MSBuildLocator.RegisterMSBuildPath(args[0]);
+            else
+                // Otherwise, just use the current directory.
+                MSBuildLocator.RegisterMSBuildPath(Directory.GetCurrentDirectory());
 
-            List<string> extra;
-            var appName = Path.GetFileName(Assembly.GetExecutingAssembly().ManifestModule.FullyQualifiedName);
+            var rpc = new JsonRpc(Console.OpenStandardOutput(), Console.OpenStandardInput(), new Program());
+            rpc.StartListening();
 
-            try
-            {
-                extra = options.Parse(args);
+            // Force resolving right-away.
+            Task.Run(Init);
 
-                if (help ||
-                    string.IsNullOrEmpty(project) ||
-                    (instance == null && msbuild == null))
-                {
-                    var writer = help ? Console.Out : Console.Error;
+            exit.Wait();
 
-                    // show some app description message
-                    writer.WriteLine($"Usage: {appName} [OPTIONS]+");
-                    writer.WriteLine();
-
-                    writer.WriteLine("Options:");
-                    options.WriteOptionDescriptions(writer);
-
-                    return -1;
-                }
-
-                // TODO: Mac support
-                if (instance != null)
-                    MSBuildLocator.RegisterInstance(instance);
-                else if (msbuild != null)
-                    MSBuildLocator.RegisterMSBuildPath(msbuild);
-
-                var metadata = await ReadProject(project, properties, output != null);
-                if (string.IsNullOrEmpty(output))
-                    metadata.Save(Console.Out);
-                else
-                    metadata.Save(output);
-
-                if (wait)
-                    Console.ReadLine();
-
-                return 0;
-            }
-            catch (OptionException e)
-            {
-                Console.Error.WriteLine($"{appName}: {e.Message}");
-                Console.Error.WriteLine($"Try '{appName} -?' for more information.");
-
-                if (wait)
-                    Console.ReadLine();
-
-                return -1;
-            }
-            catch (Exception e)
-            {
-#if DEBUG
-                if (!Debugger.IsAttached)
-                    Debugger.Launch();
-#endif
-
-                // TODO: should we render something different in this case? (non-options exception?)
-                Console.Error.WriteLine($"{appName}: {e.Message}");
-
-                if (wait)
-                    Console.ReadLine();
-
-                return -1;
-            }
+            return 0;
         }
 
-        /// <summary>
-        /// Read the given project file with the specified global 
-        /// properties.
-        /// </summary>
-        private static async Task<XElement> ReadProject(string projectFile, Dictionary<string, string> properties, bool consoleProgress)
+        private static async Task Init()
         {
-            var workspace = MSBuildWorkspace.Create(properties);
-            var project = await workspace.OpenProjectAsync(projectFile, consoleProgress ? new ConsoleProgressReporter() : null);
+            var workspace = MSBuildWorkspace.Create();
+            await workspace.OpenProjectAsync(
+                Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().ManifestModule.FullyQualifiedName), "Empty.csproj"));
+            ready = true;
+        }
+
+        private MSBuildWorkspace workspace;
+
+        public void Debug() => Debugger.Launch();
+
+        public void Exit() => exit.Set();
+
+        public bool Ping() => ready;
+
+        public void CreateWorkspace(Dictionary<string, string> properties)
+        {
+            workspace?.Dispose();
+            workspace = MSBuildWorkspace.Create(properties);
+        }
+
+        public void CloseWorkspace() => workspace?.Dispose();
+
+        public async Task<object> OpenProject(string projectFile)
+        {
+            if (workspace == null)
+                CreateWorkspace(new Dictionary<string, string>());
+
+            var project = workspace.CurrentSolution.Projects.FirstOrDefault(p => p.FilePath == projectFile) ??
+                await workspace.OpenProjectAsync(projectFile);
             var references = project.MetadataReferences.OfType<PortableExecutableReference>().ToList();
 
-            return new XElement("Project",
-                new XAttribute("Id", project.Id.Id),
-                new XAttribute("Name", project.Name),
-                new XAttribute("AssemblyName", project.AssemblyName),
-                // We may support other languages than C# in our visitors down the road.
-                new XAttribute("Language", project.Language),
-                new XAttribute("FilePath", project.FilePath),
-                new XAttribute("OutputFilePath", project.OutputFilePath),
-                new XElement("CompilationOptions",
-                    new XAttribute("OutputKind", project.CompilationOptions.OutputKind.ToString()),
-                    new XAttribute("Platform", project.CompilationOptions.Platform.ToString())),
-                new XElement("ProjectReferences", project.ProjectReferences
-                    .Where(x => workspace.CurrentSolution.Projects.Any(p => p.Id == x.ProjectId))
-                    .Select(x => new XElement("ProjectReference",
-                        new XAttribute("FilePath", workspace.CurrentSolution.Projects.First(p => p.Id == x.ProjectId).FilePath)))),
-                new XElement("MetadataReferences", references.Select(x =>
-                    new XElement("MetadataReference", new XAttribute("FilePath", x.FilePath)))),
-                new XElement("Documents", project.Documents.Select(x =>
-                    new XElement("Document",
-                        new XAttribute("FilePath", x.FilePath),
-                        new XAttribute("Folders", string.Join(Path.DirectorySeparatorChar.ToString(), x.Folders))))),
-                new XElement("AdditionalDocuments", project.AdditionalDocuments.Select(x =>
-                    new XElement("Document",
-                        new XAttribute("FilePath", x.FilePath),
-                        new XAttribute("Folders", string.Join(Path.DirectorySeparatorChar.ToString(), x.Folders)))))
-            );
-        }
-
-        private class ConsoleProgressReporter : IProgress<ProjectLoadProgress>
-        {
-            public void Report(ProjectLoadProgress loadProgress)
+            return new
             {
-                var projectDisplay = Path.GetFileName(loadProgress.FilePath);
-                if (loadProgress.TargetFramework != null)
+                project.Id.Id,
+                project.Name,
+                project.AssemblyName,
+                project.Language,
+                project.FilePath,
+                project.OutputFilePath,
+                CompilationOptions = new
                 {
-                    projectDisplay += $" ({loadProgress.TargetFramework})";
-                }
-
-                Console.WriteLine($"{loadProgress.Operation,-15} {loadProgress.ElapsedTime,-15:m\\:ss\\.fffffff} {projectDisplay}");
-            }
+                    project.CompilationOptions.OutputKind,
+                    project.CompilationOptions.Platform
+                },
+                ProjectReferences = project.ProjectReferences
+                    .Where(x => workspace.CurrentSolution.ProjectIds.Contains(x.ProjectId))
+                    .Select(x => workspace.CurrentSolution.Projects.First(p => p.Id == x.ProjectId).FilePath)
+                    .ToArray(),
+                MetadataReferences = references.Select(x => x.FilePath).ToArray(),
+                Documents = project.Documents
+                    .Select(x => new
+                    {
+                        x.FilePath,
+                        x.Folders
+                    })
+                    .ToArray(),
+                AdditionalDocuments = project.AdditionalDocuments
+                    .Select(x => new
+                    {
+                        x.FilePath,
+                        x.Folders
+                    })
+                    .ToArray()
+            };
         }
     }
 }
---
+++ src/Stunts/Stunts.Tests/ProjectReaderTests.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using StreamJsonRpc;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Stunts.Tests
+{
+    public class ProjectReaderTests : IDisposable
+    {
+#if DEBUG
+        private const string Configuration = "Debug";
+#else
+        const string Configuration = "Release";
+#endif
+        private const string ExePath = @"..\..\..\Stunts.ProjectReader\bin\" + Configuration + @"\Stunts.ProjectReader.exe";
+
+        // Generated from MSBuild with <AssemblyAttribute Include="System.Reflection.AssemblyMetadata">
+        static readonly string MSBuildBinPath = Assembly.GetExecutingAssembly()
+            .GetCustomAttributes<AssemblyMetadataAttribute>()
+            .Where(x => x.Key == "MSBuildBinPath")
+            .Select(x => x.Value)
+            .First();
+
+        ITestOutputHelper output;
+        Process process;
+
+        public ProjectReaderTests(ITestOutputHelper output)
+        {
+            this.output = output;
+            StartProcess();
+        }
+
+        [Fact]
+        public async Task WhenRunningReader_ThenCanShutdown()
+        {
+            var rpc = new JsonRpc(process.StandardInput.BaseStream, process.StandardOutput.BaseStream);
+            rpc.StartListening();
+
+            await rpc.InvokeAsync("Ping");
+            await rpc.InvokeAsync("Exit");
+
+            process.WaitForExit();
+        }
+
+        [Fact]
+        public async Task WhenReadingProject_ThenReturnsMetadata()
+        {
+            var rpc = new JsonRpc(process.StandardInput.BaseStream, process.StandardOutput.BaseStream);
+            rpc.StartListening();
+
+            await rpc.InvokeAsync("CreateWorkspace", new Dictionary<string, string>
+                {
+                    { "foo", "bar" }
+                });
+
+            var result = await rpc.InvokeAsync<dynamic>("OpenProject",
+                new FileInfo(@"..\..\Stunts.Tests.csproj").FullName);
+
+            await rpc.InvokeAsync("Exit");
+
+            process.WaitForExit();
+
+            Assert.NotNull(result);
+
+            if (Debugger.IsAttached)
+                output.WriteLine(result.ToString());
+
+            Assert.Equal("Stunts.Tests", (string)result.Name);
+        }
+
+        [Fact]
+        public async Task WhenReadingAllProjects_ThenReturnsMetadata()
+        {
+            var rpc = new JsonRpc(process.StandardInput.BaseStream, process.StandardOutput.BaseStream);
+            rpc.StartListening();
+
+            while (!await rpc.InvokeAsync<bool>("Ping"))
+            {
+                Thread.Sleep(100);
+            }
+
+            await ReadAllProjects(rpc);
+            await ReadAllProjects(rpc);
+
+            await rpc.InvokeAsync("Exit");
+
+            process.WaitForExit();
+        }
+
+        public async Task ReadAllProjects(JsonRpc rpc)
+        {
+            var total = Stopwatch.StartNew();
+            var watch = Stopwatch.StartNew();
+
+            output.WriteLine("Creating workspace");
+            await rpc.InvokeAsync("CreateWorkspace", new Dictionary<string, string>
+                {
+                    { "foo", "bar" }
+                });
+            output.WriteLine($"Created in {watch.Elapsed.TotalSeconds} seconds.");
+
+            watch.Restart();
+            output.WriteLine("Reading Stunts.csproj");
+            var result = await rpc.InvokeAsync<dynamic>("OpenProject",
+                new FileInfo(@"..\..\..\Stunts\Stunts.csproj").FullName);
+
+            output.WriteLine($"Read in {watch.Elapsed.TotalSeconds} seconds.");
+
+            watch.Restart();
+            output.WriteLine("Reading Stunts.Tests.csproj");
+            result = await rpc.InvokeAsync<dynamic>("OpenProject",
+                new FileInfo(@"..\..\Stunts.Tests.csproj").FullName);
+
+            output.WriteLine($"Read in {watch.Elapsed.TotalSeconds} seconds.");
+
+            foreach (string reference in result.ProjectReferences)
+            {
+                watch.Restart();
+                output.WriteLine($"Reading {Path.GetFileName(reference)}");
+                await rpc.InvokeAsync<dynamic>("OpenProject", reference);
+                output.WriteLine($"Read in {watch.Elapsed.TotalSeconds} seconds.");
+            }
+
+            await rpc.InvokeAsync("CloseWorkspace");
+
+            watch.Stop();
+            total.Stop();
+            output.WriteLine($"Read all in {total.Elapsed.TotalSeconds} seconds.");
+        }
+
+        public void Dispose()
+        {
+            if (!process.HasExited)
+                process.Kill();
+        }
+
+        private void StartProcess()
+            => process = Process.Start(new ProcessStartInfo(
+                new FileInfo(ExePath).FullName, (Debugger.IsAttached ? " -d" : ""))
+                {
+                    CreateNoWindow = true,
+                    WorkingDirectory = MSBuildBinPath,
+                    RedirectStandardInput = true,
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                });
+    }
+}
