c7ca8a12e5233a7b766b0fd7e3f2e0e5ca3bac6c
Daniel Cazzulino <daniel@cazzulino.com>
2019-02-24 17:35:33 -0300

    Make MockBehavior enumeration backwards compatible with v4

--- src/Moq/Moq/MockBehavior.cs
+++ src/Moq/Moq/MockBehavior.cs
@@ -1,21 +1,26 @@
-﻿namespace Moq
+﻿using System.ComponentModel;
+
+namespace Moq
 {
 	/// <summary>
 	/// Options to customize the behavior of the mock. 
 	/// </summary>
 	public enum MockBehavior
 	{
+        /// <summary>Obsolete</summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        Default = 1,
         /// <summary>
         /// Will never throw exceptions, returning default  
         /// values when necessary (null for reference types, 
         /// zero for value types and empty enumerables and arrays).
         /// </summary>
-        Loose,
+        Loose = 1,
         /// <summary>
         /// Causes the mock to always throw 
         /// an exception for invocations that don't have a 
         /// corresponding setup.
         /// </summary>
-        Strict,
+        Strict = 0,
 	}
 }
