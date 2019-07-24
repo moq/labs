eda348b0d2c457c33d6b8b66ce850d213146fc49
Daniel Cazzulino <daniel@cazzulino.com>
2019-07-24 12:44:56 -0300

    Simplify Behavior implementation by using SkipBehaviors
    Instead of duplicating behavior and having the Behavior property modify the actual pipeline, leverage the new mechanism we added to allow skipping behaviors dynamically for the current invocation.
    
    With this new feature at the pipeline level, we can create a new configuration behavior that can check the `MockBehavior` and simply skip the `StrictMockBehavior` if it's not `MockBehavior.Strict`. This greatly simplifies the implementation and makes both default and strict behaviors always present on the pipeline, but dynamically skipped based on configuration.
    
    This requires introducing a mock state-level "field", which we now save with the key `IMoq.Behavior`, which should be the guideline for extensions that need to persist state in the mock: use the `[Type].[Property]` syntax to minimize chances of collision.

---
+++ src/Moq/Moq/ConfigurePipelineBehavior.cs
@@ -0,0 +1,21 @@
+﻿using Moq.Sdk;
+using Stunts;
+
+namespace Moq
+{
+    internal class ConfigurePipelineBehavior : IStuntBehavior
+    {
+        public bool AppliesTo(IMethodInvocation invocation) => true;
+
+        public IMethodReturn Execute(IMethodInvocation invocation, GetNextBehavior next)
+        {
+            var moq = invocation.Target.AsMoq();
+            if (moq.Behavior != MockBehavior.Strict)
+            {
+                invocation.SkipBehaviors.Add(typeof(StrictMockBehavior));
+            }
+
+            return next().Invoke(invocation, next);
+        }
+    }
+}
