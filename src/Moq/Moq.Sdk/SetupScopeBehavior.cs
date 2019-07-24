78d4080e8917f16c2a4e07fcb2280628f8be2a9f
Daniel Cazzulino <daniel@cazzulino.com>
2019-07-24 12:14:35 -0300

    Add new behavior for setup scope runs
    When the pipeline is run in a `SetupScope`, we should not run the entire pipeline, but rather just the behaviors we know are safe for execution during setup, specifically `MockContextBehavior` (to set up the `MockContext.CurrentInvocation` and `MockContext.CurrentSetup`) and `DefaultValueBehavior` to return a default value for further fluent API invocations to work on.
    
    This simplifies the behaviors since they don't need to check and bail during active setup scopes, and makes them more self-contained.

---
+++ src/Moq/Moq.Sdk/SetupScopeBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Stunts;
+
+namespace Moq.Sdk
+{
+    /// <summary>
+    /// A behavior that skips all behaviors that do not apply during a setup scope.
+    /// </summary>
+    public class SetupScopeBehavior : IStuntBehavior
+    {
+        /// <summary>
+        /// Applies only if <see cref="SetupScope.IsActive"/> is <see langword="true"/>.
+        /// </summary>
+        public bool AppliesTo(IMethodInvocation invocation) => SetupScope.IsActive;
+
+        /// <summary>
+        /// Skips all non-setup behaviors from execution.
+        /// </summary>
+        public IMethodReturn Execute(IMethodInvocation invocation, GetNextBehavior next)
+        {
+            foreach (var behavior in invocation.Target.AsMock().Behaviors.Where(x => !(x is DefaultValueBehavior) && !(x is MockContextBehavior)))
+            {
+                invocation.SkipBehaviors.Add(behavior.GetType());
+            }
+
+            return next().Invoke(invocation, next);
+        }
+    }
+}
