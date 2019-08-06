d5e5fd7bfce05d388787b493ea50b212875c9fd9
Daniel Cazzulino <daniel@cazzulino.com>
2019-08-06 00:18:33 -0300

    Re-enable end to end tests for VB since they work now
    At some point these tests were not working, but as part of testing the new relocated
    override-all code fix, we found out they actually do work now again :). Made the
    dynamic stunt disposable to isolate better from one test to the next anyway, which
    should also make them more resilient by disposing the created ad-hoc workspace.

--- src/Stunts/Stunts.Tests/StuntGeneratorEndToEnd.cs
+++ src/Stunts/Stunts.Tests/StuntGeneratorEndToEnd.cs
@@ -18,171 +18,175 @@ namespace Stunts.Tests
         [Theory]
         public async Task WhenTypeIsInterface(string language)
         {
-            var dynamic = new DynamicStunt(language);
-            var target = await dynamic.CreateAsync<ICalculator>();
-            var intercepted = false;
+            using (var dynamic = new DynamicStunt(language))
+            {
+                var target = await dynamic.CreateAsync<ICalculator>();
+                var intercepted = false;
 
-            target.AddBehavior((method, next) => { intercepted = true; return next()(method, next); });
-            target.AddBehavior(new DefaultValueBehavior());
+                target.AddBehavior((method, next) => { intercepted = true; return next()(method, next); });
+                target.AddBehavior(new DefaultValueBehavior());
 
-            target.Add(1, 1);
-            Assert.True(intercepted);
-            Assert.True(intercepted, "Failed to intercept regular method");
+                target.Add(1, 1);
+                Assert.True(intercepted);
+                Assert.True(intercepted, "Failed to intercept regular method");
 
-            intercepted = false;
-            target.Clear("foo");
-            Assert.True(intercepted);
-            Assert.True(intercepted, "Failed to intercept regular method");
+                intercepted = false;
+                target.Clear("foo");
+                Assert.True(intercepted);
+                Assert.True(intercepted, "Failed to intercept regular method");
 
-            intercepted = false;
-            Assert.False(target.IsOn);
-            Assert.True(intercepted, "Failed to intercept property getter");
+                intercepted = false;
+                Assert.False(target.IsOn);
+                Assert.True(intercepted, "Failed to intercept property getter");
 
-            intercepted = false;
-            Assert.Equal(default, target.Mode);
-            Assert.True(intercepted, "Failed to intercept property getter");
+                intercepted = false;
+                Assert.Equal(default, target.Mode);
+                Assert.True(intercepted, "Failed to intercept property getter");
 
-            intercepted = false;
-            target.Mode = CalculatorMode.Scientific;
-            Assert.True(intercepted, "Failed to intercept property setter");
+                intercepted = false;
+                target.Mode = CalculatorMode.Scientific;
+                Assert.True(intercepted, "Failed to intercept property setter");
 
-            intercepted = false;
-            target.Recall("foo");
-            Assert.True(intercepted);
-            Assert.True(intercepted, "Failed to intercept regular method");
+                intercepted = false;
+                target.Recall("foo");
+                Assert.True(intercepted);
+                Assert.True(intercepted, "Failed to intercept regular method");
 
-            intercepted = false;
-            target.Store("foo", 1);
-            Assert.True(intercepted);
-            Assert.True(intercepted, "Failed to intercept regular method");
+                intercepted = false;
+                target.Store("foo", 1);
+                Assert.True(intercepted);
+                Assert.True(intercepted, "Failed to intercept regular method");
 
-            var x = 0;
-            var y = 0;
-            var z = 0;
+                var x = 0;
+                var y = 0;
+                var z = 0;
 
-            intercepted = false;
-            target.TryAdd(ref x, ref y, out z);
-            Assert.True(intercepted);
-            Assert.True(intercepted, "Failed to intercept ref/out method");
+                intercepted = false;
+                target.TryAdd(ref x, ref y, out z);
+                Assert.True(intercepted);
+                Assert.True(intercepted, "Failed to intercept ref/out method");
 
-            intercepted = false;
-            target.TurnedOn += (s, e) => { };
-            Assert.True(intercepted, "Failed to intercept event add");
+                intercepted = false;
+                target.TurnedOn += (s, e) => { };
+                Assert.True(intercepted, "Failed to intercept event add");
 
-            intercepted = false;
-            target.TurnedOn -= (s, e) => { };
-            Assert.True(intercepted, "Failed to intercept event remove");
+                intercepted = false;
+                target.TurnedOn -= (s, e) => { };
+                Assert.True(intercepted, "Failed to intercept event remove");
 
-            intercepted = false;
-            target.TurnOn();
-            Assert.True(intercepted, "Failed to intercept regular method");
+                intercepted = false;
+                target.TurnOn();
+                Assert.True(intercepted, "Failed to intercept regular method");
+            }
         }
 
-        // TODO: why does this fail for VB? 
-        //[InlineData(LanguageNames.VisualBasic)]
         [InlineData(LanguageNames.CSharp)]
+        [InlineData(LanguageNames.VisualBasic)]
         [Theory]
         public async Task WhenTypeIsAbstract(string language)
         {
-            var dynamic = new DynamicStunt(language);
-            var target = await dynamic.CreateAsync<CalculatorBase>();
-            var intercepted = false;
+            using (var dynamic = new DynamicStunt(language))
+            {
+                var target = await dynamic.CreateAsync<CalculatorBase>();
+                var intercepted = false;
 
-            target.AddBehavior((method, next) => { intercepted = true; return next()(method, next); });
-            target.AddBehavior(new DefaultValueBehavior());
+                target.AddBehavior((method, next) => { intercepted = true; return next()(method, next); });
+                target.AddBehavior(new DefaultValueBehavior());
 
-            intercepted = false;
-            Assert.False(target.IsOn);
-            Assert.True(intercepted);
-
-            intercepted = false;
-            target.TurnOn();
-            Assert.True(intercepted);
-
-            intercepted = false;
-            target.TurnedOn += (s, e) => { };
-            if (language == LanguageNames.VisualBasic)
-                // Intercepting events doesn't work in VB
-                Assert.False(intercepted, "Visual Basic can't intercept virtual events");
-            else
+                intercepted = false;
+                Assert.False(target.IsOn);
                 Assert.True(intercepted);
 
-            intercepted = false;
-            target.TurnedOn -= (s, e) => { };
-            if (language == LanguageNames.VisualBasic)
-                // Intercepting events doesn't work in VB
-                Assert.False(intercepted, "Visual Basic can't intercept virtual events");
-            else
+                intercepted = false;
+                target.TurnOn();
                 Assert.True(intercepted);
+
+                intercepted = false;
+                target.TurnedOn += (s, e) => { };
+                if (language == LanguageNames.VisualBasic)
+                    // Intercepting events doesn't work in VB
+                    Assert.False(intercepted, "Visual Basic can't intercept virtual events");
+                else
+                    Assert.True(intercepted);
+
+                intercepted = false;
+                target.TurnedOn -= (s, e) => { };
+                if (language == LanguageNames.VisualBasic)
+                    // Intercepting events doesn't work in VB
+                    Assert.False(intercepted, "Visual Basic can't intercept virtual events");
+                else
+                    Assert.True(intercepted);
+            }
         }
 
-        // TODO: why does this fail for VB? 
-        // [InlineData(LanguageNames.VisualBasic)]
+        [InlineData(LanguageNames.VisualBasic)]
         [InlineData(LanguageNames.CSharp)]
         [Theory]
         public async Task WhenTypeHasVirtualMembers(string language)
         {
-            var dynamic = new DynamicStunt(language);
-            var target = await dynamic.CreateAsync<Calculator>();
-            var intercepted = false;
+            using (var dynamic = new DynamicStunt(language))
+            {
+                var target = await dynamic.CreateAsync<Calculator>();
+                var intercepted = false;
 
-            target.AddBehavior((method, next) => { intercepted = true; return next()(method, next); });
-            target.AddBehavior(new DefaultValueBehavior());
+                target.AddBehavior((method, next) => { intercepted = true; return next()(method, next); });
+                target.AddBehavior(new DefaultValueBehavior());
 
-            target.Add(1, 1);
-            Assert.True(intercepted, "Failed to intercept regular method");
+                target.Add(1, 1);
+                Assert.True(intercepted, "Failed to intercept regular method");
 
-            intercepted = false;
-            target.Clear("foo");
-            Assert.True(intercepted, "Failed to intercept regular method");
+                intercepted = false;
+                target.Clear("foo");
+                Assert.True(intercepted, "Failed to intercept regular method");
 
-            intercepted = false;
-            Assert.False(target.IsOn);
-            Assert.True(intercepted, "Failed to intercept property getter");
+                intercepted = false;
+                Assert.False(target.IsOn);
+                Assert.True(intercepted, "Failed to intercept property getter");
 
-            intercepted = false;
-            target.Mode = CalculatorMode.Scientific;
-            Assert.True(intercepted, "Failed to intercept property setter");
+                intercepted = false;
+                target.Mode = CalculatorMode.Scientific;
+                Assert.True(intercepted, "Failed to intercept property setter");
 
-            intercepted = false;
-            Assert.Equal(default, target.Mode);
-            Assert.True(intercepted, "Failed to intercept property getter");
+                intercepted = false;
+                Assert.Equal(default, target.Mode);
+                Assert.True(intercepted, "Failed to intercept property getter");
 
-            intercepted = false;
-            target.Recall("foo");
-            Assert.True(intercepted, "Failed to intercept regular method");
+                intercepted = false;
+                target.Recall("foo");
+                Assert.True(intercepted, "Failed to intercept regular method");
 
-            intercepted = false;
-            target.Store("foo", 1);
-            Assert.True(intercepted, "Failed to intercept regular method");
+                intercepted = false;
+                target.Store("foo", 1);
+                Assert.True(intercepted, "Failed to intercept regular method");
 
-            var x = 0;
-            var y = 0;
-            var z = 0;
+                var x = 0;
+                var y = 0;
+                var z = 0;
 
-            intercepted = false;
-            target.TryAdd(ref x, ref y, out z);
-            Assert.True(intercepted, "Failed to intercept ref/out method");
+                intercepted = false;
+                target.TryAdd(ref x, ref y, out z);
+                Assert.True(intercepted, "Failed to intercept ref/out method");
 
-            intercepted = false;
-            target.TurnedOn += (s, e) => { };
-            if (language == LanguageNames.VisualBasic)
-                // Intercepting events doesn't work in VB
-                Assert.False(intercepted, "Visual Basic can't intercept virtual events");
-            else
-                Assert.True(intercepted, "Failed to intercept event add");
+                intercepted = false;
+                target.TurnedOn += (s, e) => { };
+                if (language == LanguageNames.VisualBasic)
+                    // Intercepting events doesn't work in VB
+                    Assert.False(intercepted, "Visual Basic can't intercept virtual events");
+                else
+                    Assert.True(intercepted, "Failed to intercept event add");
 
-            intercepted = false;
-            target.TurnedOn -= (s, e) => { };
-            if (language == LanguageNames.VisualBasic)
-                // Intercepting events doesn't work in VB
-                Assert.False(intercepted, "Visual Basic can't intercept virtual events");
-            else
-                Assert.True(intercepted, "Failed to intercept event remove");
+                intercepted = false;
+                target.TurnedOn -= (s, e) => { };
+                if (language == LanguageNames.VisualBasic)
+                    // Intercepting events doesn't work in VB
+                    Assert.False(intercepted, "Visual Basic can't intercept virtual events");
+                else
+                    Assert.True(intercepted, "Failed to intercept event remove");
 
-            intercepted = false;
-            target.TurnOn();
-            Assert.True(intercepted, "Failed to intercept regular method");
+                intercepted = false;
+                target.TurnOn();
+                Assert.True(intercepted, "Failed to intercept regular method");
+            }
         }
     }
 }
