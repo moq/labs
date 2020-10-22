f3d229ad31193c65836f71c3c031b025471b1502
Daniel Cazzulino <daniel@cazzulino.com>
2020-10-22 01:18:16 -0300

    Basic implementation of the static mock factory

--- src/Moq/Moq.Sdk/StaticMockFactory.cs
+++ src/Moq/Moq.Sdk/StaticMockFactory.cs
@@ -12,8 +12,21 @@ namespace Moq.Sdk
     [EditorBrowsable(EditorBrowsableState.Never)]
     public class StaticMockFactory : IMockFactory
     {
-        /// <inheritdoc />
+        /// <summary>
+        /// Uses the <see cref="MockNaming.GetFullName(Type, Type[])"/> method to 
+        /// determine the expected full type name of a compile-time generated mocks 
+        /// and tries to locate it from <paramref name="mocksAssembly"/>.
+        /// </summary>
+        /// <param name="mocksAssembly">The assembly containing the compile-time generated mocks.</param>
+        /// <param name="baseType">Base type of the mock.</param>
+        /// <param name="implementedInterfaces">Additional interfaces the mock implements.</param>
+        /// <param name="constructorArguments">Optional additional constructor arguments for the mock.</param>
         public object CreateMock(Assembly mocksAssembly, Type baseType, Type[] implementedInterfaces, object[] constructorArguments)
-            => throw new NotImplementedException();
+        {
+            var name = MockNaming.GetFullName(baseType, implementedInterfaces);
+            var type = mocksAssembly.GetType(name, true, false);
+
+            return Activator.CreateInstance(type, constructorArguments);
+        }
     }
 }
